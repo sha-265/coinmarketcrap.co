e913e80db4199de1751a037f2e1467f8c958277e
sha-265 <4103710+sha-265@users.noreply.github.com>
2019-05-26 17:04:09 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -14,50 +14,31 @@ $(document).ready(function() {
         "columns": [
             { "data": "rank" },
             { "data": "name",
-            render: function (data, type, row) {
-              img = "<img src='images/" + (data != "Bitcoin" ? 'shitcoin' : 'bitcoin') + ".png' height='32' width='32' />"
-              return img + (data != "Bitcoin" ? " Shitcoin #" + (row['rank'] - 1) : " Bitcoin")
-            }},
-            { "data": "marketCapUsd",
-              render: function (toFormat) {
-                return "$" + parseInt(toFormat).toString().replace(
-                  /\B(?=(\d{3})+(?!\d))/g, ","
-                );
+              render: function (data, type, row) {
+                img = "<img src='images/" + (data != "Bitcoin" ? 'poo' : 'bitcoin') + ".png' height='32' width='32' />"
+                return img + (data != "Bitcoin" ? " Shitcoin #" + (row['rank'] - 1) : " Bitcoin")
               }
+            },
+            { "data": "marketCapUsd",
+              "type": "num-fmt",
+              render: $.fn.dataTable.render.number( ',', '.', 0, '$' )
             },
             { "data": "priceUsd",
-              render: function (toFormat) {
-                return "$" + (Math.round(toFormat * 100) / 100).toString().replace(
-                  /\B(?=(\d{3})+(?!\d))/g, ","
-                );
-              }
+              render: $.fn.dataTable.render.number( ',', '.', 2, '$' )
             },
             { "data": "volumeUsd24Hr",
-              render: function (toFormat) {
-                return "$" + parseInt(toFormat).toString().replace(
-                  /\B(?=(\d{3})+(?!\d))/g, ","
-                );
-              }
+              render: $.fn.dataTable.render.number( ',', '.', 0, '$' )
             },
             { "data": "supply",
-              render: function (data, type, row) {
-                row['symbol'] = (row['symbol'] != "BTC" ? "SHT" : "BTC")
-                return parseInt(data).toString().replace(
-                  /\B(?=(\d{3})+(?!\d))/g, ","
-                ) + " " + row['symbol'];
-              }
+              render: $.fn.dataTable.render.number( ',', '.', 0 )
             },
             { "data": "changePercent24Hr",
-              render: function (toFormat) {
-                return (Math.round(toFormat * 100) / 100).toString().replace(
-                  /\B(?=(\d{3})+(?!\d))/g, ","
-                ) + "%";
-              }
+              render: $.fn.dataTable.render.number( ',', '.', 2, '', '%' )
             }
         ],
         "aoColumnDefs": [ {
           "aTargets": [6],
-          "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+          "fnCreatedCell": function (nTd, sData) {
              if ( sData < "0" ) {
               $(nTd).css('color', 'red')
             } else if (sData > "0") {
@@ -69,5 +50,30 @@ $(document).ready(function() {
 
     setInterval( function () {
         table.ajax.reload();
-    }, 10000 );
+    }, 30000 );
+
+    table.on('xhr', function() {
+      var ajaxJson = table.ajax.json().data;
+
+      $('#shitcoins').text(ajaxJson.length - 1);
+      $('#market-cap').text(Math.round(ajaxJson.sum('marketCapUsd'))).digits();
+      $('#volume').text(Math.round(ajaxJson.sum('volumeUsd24Hr'))).digits();
+    });
+
 } );
+
+Array.prototype.sum = function (prop) {
+    var total = 0
+    for ( var i = 0, _len = this.length; i < _len; i++ ) {
+      if (!isNaN(parseFloat(this[i][prop])))
+        total += parseFloat(this[i][prop])
+    }
+
+    return total
+}
+
+$.fn.digits = function(){
+  return this.each(function(){
+      $(this).text( $(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") );
+  })
+}
