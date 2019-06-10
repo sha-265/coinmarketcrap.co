052ef5af082d4802c02117e293ffeafcddf3152e
sha-265 <4103710+sha-265@users.noreply.github.com>
2019-06-10 18:04:42 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
             { "data": "name",
               type: 'natural',
               render: function (data, type, row) {
-                img = "<img src='images/" + (data != "Bitcoin" ? 'poo' : 'bitcoin') + ".png' height='32' width='32' />"
+                img = "<img src='images/" + (data != "Bitcoin" ? 'poo' : 'bitcoin') + ".png' height='32' width='32' alt='" + data + "' />"
                 return img + (data != "Bitcoin" ? " Shitcoin #" + (row['rank'] - 1) : " Bitcoin")
               }
             },
