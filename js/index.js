f0f160efc2f7aa88612fbb7e72a8656420471cc5
sha-265 <4103710+sha-265@users.noreply.github.com>
2019-08-01 17:14:53 +0300

    Update shitcoin rank

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var table = $('#coins').DataTable( {
-        "ajax": "https://api.coincap.io/v2/assets?limit=2000",
-        "pageLength": 100,
+        ajax: "https://api.coincap.io/v2/assets?limit=2000",
+        pageLength: 100,
         pagingType: "simple",
         scrollX: true,
         fixedColumns: {
@@ -10,46 +10,49 @@ $(document).ready(function() {
         dom: "<'row'<'col-sm-3'l><'col-sm-9'f p>>" +
              "<'row'<'col-sm-12'tr>>" +
              "<'row'<'col-sm-5'i><'col-sm-7'p>>",
-        "columns": [
-            { "data": "rank",
-              className: "text-center"
-            },
-            { "data": "name",
-              type: 'natural',
+        columns: [
+            { data: "rank",
+              className: "text-center",
               render: function (data, type, row) {
-                img = "<img src='images/" + (data != "Bitcoin" ? 'poo' : 'bitcoin') + ".png' height='32' width='32' alt='" + row['rank'] + " " + data + "' />"
-                  return img + (data != "Bitcoin" ? " Shitcoin #" + (row['rank'] - 1) : " Bitcoin")
+                return (row['name'] != "Beam" ? data : 9999 )
               }
             },
-            { "data": "marketCapUsd",
+            { data: "name",
+              type: 'natural',
+              render: function (data, type, row) {
+                img = "<img alt='" + row['rank'] + "' src='images/" + (data != "Bitcoin" ? 'poo' : 'bitcoin') + ".png' height='32' width='32' title='" + data + " " + row['symbol'] + "' />"
+                  return img + " " + (data != "Bitcoin" ? "Shitcoin #" + (row['rank'] - 1) : data)
+              }
+            },
+            { data: "marketCapUsd",
               className: "text-right",
               render: $.fn.dataTable.render.number( ',', '.', 0, '$' ),
               defaultContent: "$0"
             },
-            { "data": "priceUsd",
+            { data: "priceUsd",
               className: "text-right",
               render: $.fn.dataTable.render.number( ',', '.', 2, '$' ),
               defaultContent: "$0.00"
             },
-            { "data": "volumeUsd24Hr",
+            { data: "volumeUsd24Hr",
               className: "text-right",
               render: $.fn.dataTable.render.number( ',', '.', 0, '$' ),
               defaultContent: "$0"
             },
-            { "data": "supply",
+            { data: "supply",
               className: "text-right",
               render: $.fn.dataTable.render.number( ',', '.', 0 ),
               defaultContent: "-"
             },
-            { "data": "changePercent24Hr",
+            { data: "changePercent24Hr",
               className: "text-right",
               render: $.fn.dataTable.render.number( ',', '.', 2, '', '%' ),
               defaultContent: "0.00%"
             }
         ],
-        "aoColumnDefs": [ {
-          "aTargets": [6],
-          "fnCreatedCell": function (nTd, sData) {
+        aoColumnDefs: [ {
+          aTargets: [6],
+          fnCreatedCell: function (nTd, sData) {
              if ( sData < "0" ) {
               $(nTd).css('color', 'red')
             } else if (sData > "0") {
